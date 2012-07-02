348b56cd7c30bb0acc952068e571acc48be62a0e
russell <russellofallion@gmail.com>
2012-07-02 15:19:41 -0700

    Moved the config files and updated the configfetcher

--- tottest/lexicographers/constants.py
+++ tottest/lexicographers/constants.py
@@ -8,5 +8,5 @@ TIME_STAMP_FOLDER = "{name}_{stamp}"
 TIME_FORMAT = "%Y_%m_%d_%a"
 
 
-CONFIG_FOLDER = "config"
-DEFAULTS = ["tot.ini", 'readme_useme_loveme']
+CONFIG_FOLDER = "lexicographers/configfiles"
+DEFAULTS = ["throughputovertime.ini"]
