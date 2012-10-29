02c75fb187f7d7218c9c8dc0e8c606c7646fdb2b
russell <necromuralist@gmail.com>
2012-10-28 18:54:43 -0700

    Committing license

--- tottest/watchers/timestamp.py
+++ tottest/watchers/timestamp.py
@@ -2,6 +2,14 @@
 A module to store common timestamp formats
 """
 
+class TimestampFormatEnum(object):
+    """
+    A class to hold the format names
+    """
+    __slots__ = ()
+    iperf = "iperf"
+# end class TimestampFormatEnums
+
 class TimestampFormat(object):
     """
     A class to hold timestampformats
