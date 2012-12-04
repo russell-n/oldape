d3d0efafa7e48bfcf57e5db653fb748f8baebdae
russell <rhnakamu@cecs.pdx.edu>
2012-12-04 14:05:02 -0800

    Changed the ValidatingOutput to return a space on socket timeout so that a consumer of the lines won't be blocked forever.

--- tottest/commands/poweron.py
+++ tottest/commands/poweron.py
@@ -30,14 +30,14 @@ class PowerOn(BaseClass):
         self.logger.info("Turning on {0} (switch '{1}')".format(params.identifier, params.switch))
         self.switches[params.identifier](params.switch)
         #self.switches[identifier].close()
-        return "id_{0}_switch_{1}".format(params.identifier, params.switch)
+        return "id_{0}_switch_{1}".format(params.identifier, params.switch.zfill(2))
 
     def turn_all_off(self):
         """
         :postcondition: all switches turned off       
         """
         for name,switch in self.switches.iteritems():
-            self.logger.info("Turning off switches on {0}".format(name))
+            self.logger.info("Turning off {1} on {0}".format(switch, name))
             switch.all_off()
         return
 # end class PowerOn
